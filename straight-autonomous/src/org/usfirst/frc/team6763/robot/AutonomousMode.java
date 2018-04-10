4303a930504bb248eb30eae333bf90c6adf89c1e
cjdenio <34525547+cjdenio@users.noreply.github.com>
2018-04-10 18:49:52 -0400

    Added DRIVE_BACKWARD and LOWER_LIFT after autonomous modes

--- straight-autonomous/src/org/usfirst/frc/team6763/robot/AutonomousMode.java
+++ straight-autonomous/src/org/usfirst/frc/team6763/robot/AutonomousMode.java
@@ -18,7 +18,9 @@ public interface AutonomousMode {
 			new Instruction(State.RAISE_LIFT, 1, 0),
 			new Instruction(State.DRIVE_FORWARD, 8, 45),
 			new Instruction(State.WAIT, 1, 0),
-			new Instruction(State.EJECT_CUBE, 1, 0)));
+			new Instruction(State.EJECT_CUBE, 1, 0),
+			new Instruction(State.DRIVE_BACKWARD, 4, 0),
+			new Instruction(State.LOWER_LIFT, 0.9, 0)));
 	
 	List<Instruction> switchLeftPositionCenter = new ArrayList<Instruction>(Arrays.asList(
 			new Instruction(State.DRIVE_FORWARD, 94, -50),
@@ -26,7 +28,9 @@ public interface AutonomousMode {
 			new Instruction(State.RAISE_LIFT, 1.2, 0),
 			new Instruction(State.DRIVE_FORWARD, 8, 0),
 			new Instruction(State.WAIT, 1, 0),
-			new Instruction(State.EJECT_CUBE, 1, 0)));
+			new Instruction(State.EJECT_CUBE, 1, 0),
+			new Instruction(State.DRIVE_BACKWARD, 4, 0),
+			new Instruction(State.LOWER_LIFT, 1.08, 0)));
 	
 	List<Instruction> switchLeftPositionRight = new ArrayList<Instruction>(Arrays.asList(
 			new Instruction(State.DRIVE_FORWARD, 60, 0),
@@ -35,7 +39,9 @@ public interface AutonomousMode {
 			new Instruction(State.SPIN_RIGHT, 0, 0),
 			new Instruction(State.RAISE_LIFT, 1, 0),
 			new Instruction(State.DRIVE_FORWARD, 29, 0),
-			new Instruction(State.EJECT_CUBE, 1, 0)));
+			new Instruction(State.EJECT_CUBE, 1, 0),
+			new Instruction(State.DRIVE_BACKWARD, 4, 0),
+			new Instruction(State.LOWER_LIFT, 0.9, 0)));
 	
 	List<Instruction> switchRightPositionLeft = new ArrayList<Instruction>(Arrays.asList(
 			new Instruction(State.DRIVE_FORWARD, 60, 0),
@@ -44,7 +50,9 @@ public interface AutonomousMode {
 			new Instruction(State.TURN_LEFT, 0, 0),
 			new Instruction(State.RAISE_LIFT, 1, 0),
 			new Instruction(State.DRIVE_FORWARD, 29, 0),
-			new Instruction(State.EJECT_CUBE, 1, 0)));
+			new Instruction(State.EJECT_CUBE, 1, 0),
+			new Instruction(State.DRIVE_BACKWARD, 4, 0),
+			new Instruction(State.LOWER_LIFT, 0.9, 0)));
 	
 	List<Instruction> switchRightPositionCenter = new ArrayList<Instruction>(Arrays.asList(
 			new Instruction(State.DRIVE_FORWARD, 52, 35),
@@ -52,7 +60,9 @@ public interface AutonomousMode {
 			new Instruction(State.RAISE_LIFT, 1, 0),
 			new Instruction(State.DRIVE_FORWARD, 20, 0),
 			new Instruction(State.WAIT, 1, 0),
-			new Instruction(State.EJECT_CUBE, 1, 0)));
+			new Instruction(State.EJECT_CUBE, 1, 0),
+			new Instruction(State.DRIVE_BACKWARD, 4, 0),
+			new Instruction(State.LOWER_LIFT, 0.9, 0)));
 	
 	List<Instruction> switchRightPositionRight = new ArrayList<Instruction>(Arrays.asList(
 			new Instruction(State.DRIVE_FORWARD, 75, 0),
@@ -60,13 +70,17 @@ public interface AutonomousMode {
 			new Instruction(State.RAISE_LIFT, 1, 0),
 			new Instruction(State.DRIVE_FORWARD, 8, -45),
 			new Instruction(State.WAIT, 1, 0),
-			new Instruction(State.EJECT_CUBE, 1, 0)));
+			new Instruction(State.EJECT_CUBE, 1, 0),
+			new Instruction(State.DRIVE_BACKWARD, 4, 0),
+			new Instruction(State.LOWER_LIFT, 0.9, 0)));
 	
 	List<Instruction> scaleLeftPositionLeft = new ArrayList<Instruction>(Arrays.asList(
 			new Instruction(State.DRIVE_FORWARD, 230, 0),
 			new Instruction(State.SPIN_RIGHT, 0, 30),
 			new Instruction(State.RAISE_LIFT, 2.5, 0),
-			new Instruction(State.EJECT_CUBE, 1, 0)));
+			new Instruction(State.EJECT_CUBE, 1, 0),
+			new Instruction(State.DRIVE_BACKWARD, 1, 0),
+			new Instruction(State.LOWER_LIFT, 2.25, 0)));
 	
 	List<Instruction> scaleLeftPositionCenter = new ArrayList<Instruction>(Arrays.asList(
 			new Instruction(State.DRIVE_FORWARD, 36, 0)));
@@ -78,7 +92,9 @@ public interface AutonomousMode {
 			new Instruction(State.TURN_RIGHT, 0, 13),
 			new Instruction(State.DRIVE_FORWARD, 5, 13),
 			new Instruction(State.RAISE_LIFT, 2.5, 0),
-			new Instruction(State.EJECT_CUBE, 1, 0)));
+			new Instruction(State.EJECT_CUBE, 1, 0),
+			new Instruction(State.DRIVE_BACKWARD, 1, 0),
+			new Instruction(State.LOWER_LIFT, 2.25, 0)));
 	
 	List<Instruction> scaleRightPositionLeft = new ArrayList<Instruction>(Arrays.asList(
 			new Instruction(State.DRIVE_FORWARD, 200, 0),
@@ -87,7 +103,9 @@ public interface AutonomousMode {
 			new Instruction(State.SPIN_LEFT, 0, -13),
 			new Instruction(State.DRIVE_FORWARD, 5, 13),
 			new Instruction(State.RAISE_LIFT, 2.5, 0),
-			new Instruction(State.EJECT_CUBE, 1, 0)));
+			new Instruction(State.EJECT_CUBE, 1, 0),
+			new Instruction(State.DRIVE_BACKWARD, 1, 0),
+			new Instruction(State.LOWER_LIFT, 2.25, 0)));
 	
 	List<Instruction> scaleRightPositionCenter = new ArrayList<Instruction>(Arrays.asList(
 			new Instruction(State.DRIVE_FORWARD, 36, 0)));
@@ -96,7 +114,9 @@ public interface AutonomousMode {
 			new Instruction(State.DRIVE_FORWARD, 230, 0),
 			new Instruction(State.TURN_LEFT, 0, -30),
 			new Instruction(State.RAISE_LIFT, 2.5, 0),
-			new Instruction(State.EJECT_CUBE, 1, 0)));
+			new Instruction(State.EJECT_CUBE, 1, 0),
+			new Instruction(State.DRIVE_BACKWARD, 1, 0),
+			new Instruction(State.LOWER_LIFT, 2.25, 0)));
 	
 	List<Instruction> stop = new ArrayList<Instruction>(Arrays.asList(new Instruction(State.STOP, 0, 0)));
 }
